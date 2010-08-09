e5416c59d126965d22760475366c56382ee72e18
cohitre <carlosrr@gmail.com>
2010-08-09 15:25:10 -0700

    Hosting the css file

--- helvetireddit.user.js
+++ helvetireddit.user.js
@@ -9,19 +9,10 @@
 // @author      helvetireddit by cohitre. Inspired by Helvetireader by Jon Hicks (http://www.hicksdesign.co.uk).
 // ==/UserScript==
 
-/*
-var favvy = document.createElement('link');
-favvy.setAttribute('type', 'image/x-icon');
-favvy.setAttribute('rel', 'shortcut icon');
-favvy.setAttribute('href', 'http://localhost:8000/favicon.png');
-var head = document.getElementsByTagName('head')[0];
-head.appendChild(favvy);
-*/
-
 var cssNode = document.createElement('link');
 cssNode.type = 'text/css';
 cssNode.rel = 'stylesheet';
-cssNode.href = 'http://localhost:8000/css/helvetireddit.1.css';
+cssNode.href = 'http://assets.cohitre.com/helvetireddit/css/helvetireddit.1.css';
 cssNode.media = 'screen';
 cssNode.title = 'dynamicLoadedSheet';
 document.getElementsByTagName("head")[0].appendChild(cssNode);
